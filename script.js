53bb1489b39398d1040ba86bf8a9145063cd64b7
Tilen Marš <marstilen@gmail.com>
2022-11-30 18:19:17 +0100

    try to change background on hover

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-let divElement = document.createElement("div");
 let divContainer = document.getElementById("div-container");
 for (let i = 0; i < 256; i++) {
-  divContainer.innerHTML += "<div class='field'></div>";
+  divContainer.innerHTML += `<div class='field'></div>`;
 }
+
+const containers = document.querySelectorAll(".field");
+
+containers.forEach((f) =>
+  f.addEventListener("mouseenter", function () {
+    console.log(f);
+    containers.forEach((e) => {
+      var div = e.querySelector("div");
+      div.classList.add("hidden");
+      div.classList.remove("show");
+    });
+    this.querySelector("div").classList.add("black-div");
+  })
+);
+
+/*const fields = document.getElementsByClassName("field");
+console.log(fields);
+
+fields.forEach((f) => {
+  f.addEventListener("mouseenter", function () {
+    var div = f.querySelector("div");
+    f.classList.add("black-div");
+  });
+});
+*/
