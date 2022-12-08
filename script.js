c807172c9576d86889614158322c726d7021a77e
Tilen Marš <marstilen@gmail.com>
2022-12-08 13:46:50 +0000

    try changing mouseenter to click

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let fields = [];
 for (let i = 0; i < 256; i++) {
   var gridElement = document.createElement('div');
   gridElement.className =  'grid-element';
-  gridElement.addEventListener('mouseenter', function (e) {
+  gridElement.addEventListener('click', function (e) {
     e.target.style.backgroundColor = 'black';
   })
   divContainer.appendChild(gridElement);
