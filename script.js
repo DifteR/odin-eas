4a5d2a5771c41da40cdfd7eb2ab746e83c9e4579
Tilen Marš <marstilen@gmail.com>
2022-12-15 17:11:26 +0100

    Force elements into their own rows

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let divContainer = document.getElementById("div-container");
 let idFinder;
 let fields = [];
-for (let i = 0; i < 256; i++) {
-  var gridElement = document.createElement("div");
-  gridElement.className = "grid-element";
-  gridElement.addEventListener("mouseenter", function (e) {
-    e.target.style.backgroundColor = "black";
-  });
-  divContainer.appendChild(gridElement);
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    var gridElement = document.createElement("div");
+    gridElement.className = "grid-element";
+    gridElement.addEventListener("mouseenter", function (e) {
+      e.target.style.backgroundColor = "black";
+    });
+    divContainer.appendChild(gridElement);
+  }
+  var newBreak = document.createElement("div");
+  gridElement.className = "break";
 }
