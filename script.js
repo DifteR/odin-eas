419aa8d1a048a6e9bad67f5adb0649bd24d0d062
Tilen Marš <marstilen@gmail.com>
2022-12-20 08:43:29 +0100

    Add button to change grid size

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let divContainer = document.getElementById("div-container");
 let idFinder;
+let size;
 let fields = [];
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    var gridElement = document.createElement("div");
-    gridElement.className = "grid-element";
-    gridElement.addEventListener("mouseenter", function (e) {
-      e.target.style.backgroundColor = "black";
-    });
-    divContainer.appendChild(gridElement);
+
+function drawDivs(size) {
+  divContainer.replaceChildren();
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j <= size; j++) {
+      var gridElement = document.createElement("div");
+      gridElement.className = "grid-element";
+      gridElement.addEventListener("mouseenter", function (e) {
+        e.target.style.backgroundColor = "black";
+      });
+      divContainer.appendChild(gridElement);
+    }
+    var newBreak = document.createElement("div");
+    gridElement.className = "break";
   }
-  var newBreak = document.createElement("div");
-  gridElement.className = "break";
 }
+
+drawDivs(16);
+
+document
+  .getElementById("grid-size")
+  .addEventListener("click", () => drawDivs(prompt()));
